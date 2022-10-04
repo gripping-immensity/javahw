a4ba9718faadcc96d737bd72a6b038e570515226
Evgeniy <gripping.immensity@gmail.com>
2022-10-04 21:08:39 +0300

    init

---
+++ hw01-gradle/src/main/java/ru/otus/HelloOtus.java
@@ -0,0 +1,23 @@
+package ru.otus;
+
+import com.google.common.base.Charsets;
+import com.google.common.hash.BloomFilter;
+import com.google.common.hash.Funnels;
+
+public class HelloOtus {
+    public static void main(String... args) {
+        System.out.println("Hello !");
+        BloomFilter<String> words = BloomFilter.create(Funnels.stringFunnel(Charsets.UTF_16), 100, 0.01);
+        words.put("ruse");
+        words.put("ubiquitous");
+        words.put("hello");
+
+        System.out.println("Checkin \"yes\" status in hashmap : ");
+
+        if (words.mightContain("yes")) {
+            System.out.println("maybe");
+        } {
+            System.out.println("definitely not");
+        }
+    }
+}
